7d25ec9da6e77977f45c746bdfb32ee313c04679
dami-akinbi <akinbidamilola074@gmail.com>
2021-06-25 01:09:38 +0100

    a lot of proof-reading, had to do hard refresh. Learned up to video 30, Styling the App 1

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Article
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
 
--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
@@ -8,8 +8,8 @@ from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('about/', views.about),
     path('', article_views.article_list, name="home"),
 ]
